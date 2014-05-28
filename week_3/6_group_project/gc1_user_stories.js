bc79c26c2789a0e2e0ef94f869cfeede4149b4c7
Joey Sabani <joey.sabani@gmail.com>
2014-05-28 20:49:49 +0200

    renamed file and added my solution

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,21 @@
+// My role in the group is: Translate tests into user stories
+
+// Here is my part of the challenge:
+
+// Test for sums
+
+// I want to take numbers from a set of numbers and add them all together. The list of numbers can have any amount
+// of numbers in it. The test will take the first number from the list and add it to the total within itself. The test
+// will continue to add the next number in the list to the previous total until there are no more numbers in the list.
+// The value, after all numbers in the list are added, will be our sum.
+
+// test for median
+// I want to order a list of numbers from smallest to largest. After the list has been ordered, I want to find the
+// middle of the list by taking the total numbers in the list and dividing by two. The test should be able to return
+// return the middle for all odd numbered lists by finding out if dividing by two doesn't produce a whole number.
+// If a whole number isn't found, or there is a reaminder when dividing, it should just look for the middle number in
+// our list. 
+
+// test for mean
+// I want to take the sum of all numbers in the list, by using the test for adding all numbers together until there
+// are no numbers left. Once I have found that number, I want to divide the sum by the amount of numbers in the list.
